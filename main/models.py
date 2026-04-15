460cb8ad986e09f3c4a73600ff0f0ab334cab24d
Samandar Abdurahimov <hasanovayoqutxon56@gmail.com>
2026-04-15 08:49:21 +0500

    Implement __str__ and __repr__ methods in models
    Added string representation methods for Category and News models.

--- main/models.py
+++ main/models.py
@@ -3,7 +3,10 @@ from datetime import  datetime
 
 class Category(models.Model):
     name=models.CharField(max_length=110, unique=True)
-
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return f"Pk: {self.pk}. Name {self.name}"
 class News(models.Model):
     name=models.CharField(max_length=100)
     text=models.TextField()
@@ -11,4 +14,7 @@ class News(models.Model):
     views=models.IntegerField(default=0)
     created = models.DateTimeField(default=datetime.now)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return f"Pk: {self.pk}. Name {self.name}"
